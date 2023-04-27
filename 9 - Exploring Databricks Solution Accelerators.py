659506f047f59b1d8c8c1379147ac5c53bdbc462
ronguerrero <ronguerrero>
2023-04-27 12:48:54 +0000

    Notebook renumbering

---
+++ 9 - Exploring Databricks Solution Accelerators.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC Check out the list of Databricks Solution Accelerators here: https://www.databricks.com/solutions/accelerators
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC For the demo, let's look at the Anti-Money Laundering Solution Accelerator - https://github.com/databricks-industry-solutions/anti-money-laundering
+
+# COMMAND ----------
+
+# DBTITLE 0,On the left menu, select "Repos" and then click on "Add Repo"
+# MAGIC %md
+# MAGIC <img src ='https://github.com/ronguerrero/hadoop-migration-workshop/raw/main/resources/Add%20Repo.png'>
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Add the following Git Repository URL (copy-paste from the link here):  https://github.com/databricks-industry-solutions/anti-money-laundering
+# MAGIC %md
+# MAGIC <img src ='https://github.com/ronguerrero/hadoop-migration-workshop/raw/main/resources/Solution%20Accelerator%20.png'>
+
+# COMMAND ----------
+
+
